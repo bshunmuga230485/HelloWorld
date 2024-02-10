33d88cbbbe9e4963a9cb93df59041136f40decb4
shunmugasundaram baskaran <39426451+sbaskaran0423@users.noreply.github.com>
2024-02-10 22:20:27 +0530

    my 3rd commit

--- quickstart/src/main/java/com/learnsb/quickstart/HelloWorldController.java
+++ quickstart/src/main/java/com/learnsb/quickstart/HelloWorldController.java
@@ -8,6 +8,6 @@ public class HelloWorldController {
 
     @GetMapping(path = "/hello")
     public String helloWorld() {
-        return "Hello My Peaceful World ShunmugaSundaram";
+        return "Hello My Peaceful World ShunmugaSundaram Bharathi Daddy";
     }
 }
